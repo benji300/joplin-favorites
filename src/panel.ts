5b0fedee87f9330971a3c3f56e1a9a19171fcba4
Henry Heino <personalizedrefrigerator@gmail.com>
2023-11-13 09:08:33 -0800

    Fix click listeners not working: Use readonly instead of disabled for inputs

--- src/panel.ts
+++ src/panel.ts
@@ -100,7 +100,7 @@ export class Panel {
           style="height:${this._settings.lineHeight}px;min-width:${this._settings.minFavWidth}px;max-width:${this._settings.maxFavWidth}px;background:${bg};border-color:${dividerColor};">
           <span class="favorite-inner" style="border-color:${dividerColor};">
             ${typeIconHtml}
-            <input class="title" title="${favorite.title}" value="${favorite.title}" style="color:${fg};" disabled></input>
+            <input class="title" title="${favorite.title}" value="${favorite.title}" style="color:${fg};" readonly></input>
             <span class="controls" style="background:${hoverBg};">
               <span class="rename fas fa-pen" title="Rename" style="color:${fg};"></span>
               <span class="delete fas fa-trash" title="Delete" style="color:${fg};"></span>
